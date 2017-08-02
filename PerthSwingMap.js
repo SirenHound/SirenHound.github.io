b1237df7836bd12843dc2bb3353634e0e2d71421
Ethan McCoy <SirenHound@users.noreply.github.com>
2017-08-02 10:11:04 +0800

    Update PerthSwingMap.js

--- PerthSwingMap.js
+++ PerthSwingMap.js
@@ -1,4 +1,4 @@
-var map = L.map('danceMap').setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
+var map = L.map('mapId').setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
