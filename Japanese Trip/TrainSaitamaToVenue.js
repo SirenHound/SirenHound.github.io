4bec7702b0d50ea53612892e3df28acd01e52595
Ethan McCoy <SirenHound@users.noreply.github.com>
2017-09-18 16:26:58 +0800

    Create TrainSaitamaToVenue.js

---
+++ Japanese Trip/TrainSaitamaToVenue.js
@@ -0,0 +1,17 @@
+var map = L.map('mapId');//.setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+if ("geolocation" in navigator) {
+  /* geolocation is available */
+  navigator.geolocation.getCurrentPosition(function(position) {
+    //alert("L");
+    var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+    //alert("latlng: "+ latLng.toString());
+    L.circleMarker(latLng, {}).addTo(map);
+    map.panTo(latLng);
+  });
+} else {
+  /* geolocation IS NOT available */
+  alert("No Location");
+}  
